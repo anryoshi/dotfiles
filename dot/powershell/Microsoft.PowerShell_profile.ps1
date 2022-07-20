945f49fb3030872d99e11e7ad30e7660b434ac9b
Anton Rybakov <anryoshi@gmail.com>
2022-07-20 21:27:46 +0200

    Vault Powershell functions; and a couple of options

--- dot/powershell/Microsoft.PowerShell_profile.ps1
+++ dot/powershell/Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,7 @@
 #     starship - as cross-platform prompt
 
 Set-PSReadLineOption -EditMode Emacs
+Set-PSReadlineOption -BellStyle None
 
 function Enter-DevEnv {
     if (!(Get-Command vswhere -ErrorAction 'SilentlyContinue')) {
@@ -38,6 +39,55 @@ function Enter-DevEnv {
 
     Import-Module $devShellDllPath
     Enter-VsDevShell -SkipAutomaticLocation -DevCmdArguments "-arch=x64" $instanceId
+}
+
+function Write-CompilerEnvironment {
+    [ordered]@{ CC = $env:CC; CC_LD = $env:CC_LD } | Format-Table -AutoSize -HideTableHeaders
+}
+
+enum Compiler {
+    msvc
+    clang
+    clang_cl
+}
+
+function Set-CompilerEnvironment ([Compiler]$compenv = [Compiler]::msvc) {
+    $envs = @{
+        [Compiler]::msvc = @("cl", "link")
+        [Compiler]::clang = @("clang", "lld")
+        [Compiler]::clang_cl = @("cland-cl", "lld-link")
+    }
+
+    $to_set = $envs[$compenv]
+
+    $env:CC = $to_set[0]
+    $env:CC_LD = $to_set[1]
+
+    Write-CompilerEnvironment
+}
+
+function Unlock-Vault ([string]$Path, [bool]$Silent = $false) {
+    Get-ChildItem -Path $Path -File -Include '*.asc' -Recurse `
+        | Foreach-Object {
+            $EncryptedFile = $_.FullName
+            $DecryptedFile = Join-Path $_.DirectoryName $_.BaseName
+            If (-Not $Silent) {
+                Write-Output "Decrypting`n`t<- $EncryptedFile`n`t-> $DecryptedFile"
+            }
+            gpg --yes --decrypt --output $DecryptedFile $EncryptedFile
+          }
+}
+
+function Lock-Vault ([string]$Path, [bool]$Silent = $false) {
+    Get-ChildItem -Path $Path -File -Exclude '*.asc' -Recurse `
+        | Foreach-Object {
+            $DecryptedFile = $_.FullName
+            $EncryptedFile = $DecryptedFile + '.asc'
+            If (-Not $Silent) {
+                Write-Output "Encrypting`n`t<- $DecryptedFile`n`t-> $EncryptedFile"
+            }
+            gpg --yes --encrypt --armor --recipient anryoshi@gmail.com --output $EncryptedFile $DecryptedFile
+          }
 }
 
 Invoke-Expression (&starship init powershell)
