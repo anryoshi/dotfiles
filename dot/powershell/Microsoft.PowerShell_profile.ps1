9469fefc282585c7f34d8967d7a4bf9392eacdd8
Anton Rybakov <anryoshi@gmail.com>
2024-11-21 21:42:36 +0100

    Fix for pwsh 7.2 light background color

--- dot/powershell/Microsoft.PowerShell_profile.ps1
+++ dot/powershell/Microsoft.PowerShell_profile.ps1
@@ -102,3 +102,14 @@ New-Alias -Name "l" Get-ChildItemColor
 
 Invoke-Expression (&scoop-search --hook)
 Invoke-Expression (&starship init powershell)
+
+# https://superuser.com/questions/1756130/change-color-of-powershell-7-get-childitem-result
+
+Set-PSReadLineOption -Colors @{
+    Parameter            = 'Blue'
+    String               = 'DarkMagenta'
+    Operator             = 'DarkCyan'
+    Variable             = 'DarkYellow'
+    Command              = 'DarkGray'
+    Default              = 'Black'
+}
