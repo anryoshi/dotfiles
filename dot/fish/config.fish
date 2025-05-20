dda33118ecee108df0422c65237b2dc6cd570372
Anton Rybakov <anryoshi@gmail.com>
2025-05-20 09:05:15 +0000

    Tunning config.fish and vimrc

--- dot/fish/config.fish
+++ dot/fish/config.fish
@@ -14,14 +14,18 @@ function readd_on_top_of_path \
   fish_add_path -P $entry
 end
 
-function init_env -a env_name
+function configure_environment_manager \
+    -d "Configure tool (e.g. rbenv, pyenv...) in the unified way " \
+    -a env_name
   set -f env_root (string upper $env_name)_ROOT
   set -gx $env_root "$HOME/.local/env/$env_name"
+  type -q $env_name
+  and mkdir -p $env_root
   if test -d $$env_root
-    readd_on_top_of_path "$$env_root/bin"
-    if type -q $env_name
-      $env_name init - | source
-    end
+    type -q $env_name
+    or readd_on_top_of_path "$$env_root/bin"
+    type -q $env_name
+    and $env_name init - | source
   else
     for var in (set -n)
       string match -q (string upper $env_name)'_*' -- $var
@@ -34,13 +38,13 @@ end
 #       already set pathes by the parent shell processes
 #       create review for the rbenv with fix for this
 remove_from_path "/home/$USER/.rbenv/shims"
-init_env rbenv
+configure_environment_manager rbenv
 
-init_env pyenv
+configure_environment_manager pyenv
 
 set -gx GOENV_GOPATH_PREFIX "$HOME/.local/go"
 set -gx GOENV_PATH_ORDER "front"
-init_env goenv
+configure_environment_manager goenv
 
 readd_on_top_of_path "$HOME/.cargo/bin"
 readd_on_top_of_path "$HOME/.bin"
